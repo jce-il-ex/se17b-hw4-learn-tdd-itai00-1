1e41809093828ec32fddb66458e2dbd61d306ca4
Itai <itaigaf@gmail.com>
2017-05-21 09:17:45 +0300

    RED

--- test/test.js
+++ test/test.js
@@ -16,3 +16,7 @@ C.getChange = function () {    // enough to satisfy the test
     return true;               // also passes JSLint
 };
 module.exports = C;            // export the module with a single method
+
+it('getChange(210,300) should equal [50,20,20]', function(){
+    assert.deepEqual(C.getChange(210,300), [50,20,20]);
+})
